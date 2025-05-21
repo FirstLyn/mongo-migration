057a0d1a9d87260836320ed1c45e5ddd77fb8743
FirstLyn <firstlyn.corp@gmail.com>
2025-05-21 18:05:06 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,6 @@ const path = require("path");
 
 const config = {
   mongoUri: process.env.MONGO_URI,
-  dbName: process.env.MONGO_DB_NAME || "test_db",
   dryRun: process.env.DRY_RUN === "true",
   migrationDir: path.resolve(__dirname, "../migrations"),
   dataDir: path.resolve(__dirname, "../data"),
