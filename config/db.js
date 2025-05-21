631ced3d87f70efdd5b9a32e4fdf2c994002e66f
FirstLyn <firstlyn.corp@gmail.com>
2025-05-21 18:06:42 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require("mongodb");
 const config = require("./config");
 
-async function connectToDB() {
+async function connectToDB(dbName) {
   const client = new MongoClient(config.mongoUri);
   await client.connect();
-  return { client, db: client.db(config.dbName) };
+  return { client, db: client.db(dbName) };
 }
 
 module.exports = { connectToDB };
