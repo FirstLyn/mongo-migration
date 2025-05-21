2c74b91bc3385bda089fdffaedc970e2ac51961c
FirstLyn <firstlyn.corp@gmail.com>
2025-05-21 18:04:24 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
+// settings/config.js
 require("dotenv").config();
 const path = require("path");
 
 const config = {
   mongoUri: process.env.MONGO_URI,
-  dbName: "test_db",
+  dbName: process.env.MONGO_DB_NAME || "test_db",
   dryRun: process.env.DRY_RUN === "true",
   migrationDir: path.resolve(__dirname, "../migrations"),
   dataDir: path.resolve(__dirname, "../data"),
